e6810997231cd22765d5e149de07094c3cdadedd
TMIPlanilla <tecnomedicaform@gmail.com>
2025-03-25 10:46:33 -0600

    🔐 Usar variable de entorno CREDENTIALS_JSON para autenticación segura

--- backend/autenticacion.js
+++ backend/autenticacion.js
@@ -1,10 +1,7 @@
 const { google } = require('googleapis');
-const fs = require('fs');
-const path = require('path');
 
 const auth = async () => {
-  const CREDENTIALS_PATH = path.join(__dirname, 'credentials.json');
-  const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, 'utf8'));
+  const credentials = JSON.parse(process.env.CREDENTIALS_JSON);
 
   const authClient = new google.auth.JWT(
     credentials.client_email,
